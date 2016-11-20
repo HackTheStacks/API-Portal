cefda2af6610f3d49e10b6ee2cf5dcb4fd09cfe3
Joseph Spens <joseph@spens.us>
2016-11-20 12:35:27 -0500

    Whitelist docs for public use

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // CORS so other teams can ping us
 // http://enable-cors.org/server_expressjs.html
